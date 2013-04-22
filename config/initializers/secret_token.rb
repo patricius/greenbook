f1ebf27514bfaca002c6397d1c3c35e23bbd3fed
pATRICIUS <patricius@github.com>
2013-04-22 00:14:31 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greenbook::Application.config.secret_token = '11a7eb3412e77aa0aad054361b9ab436523e9ed602d730b7de88f0f68688662ae2ef2bb63d36071a9a952556f90ea2325148d76949d446788ffeeb8a48ddbb09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Greenbook::Application.config.session_store :cookie_store, key: '_greenbook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Greenbook::Application.config.session_store :active_record_store
